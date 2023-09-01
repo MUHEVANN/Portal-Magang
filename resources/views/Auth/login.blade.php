9efcb21cd075dc76870cfa853ad567291e861dc5
HanifNAfrian <hanifnafrian12@gmail.com>
2023-09-01 16:15:38 +0700

    login ui and tailwind vite

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -6,29 +6,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    @vite('resources/css/app.css')
 </head>
 
-<body>
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-    <form action="{{ url('login') }}" method="post" style="display: flex;flex-direction:column;">
+<body class="bg-slate-100 w-full h-screen flex justify-center align-middle">
+    <form action="{{ url('login') }}" method="post" class="bg-white self-center p-20  sm:w-[40rem]">
         @csrf
-        <form-group style="display: flex">
-            <label for="email">Email</label>
-            <input type="email" name="email">
-        </form-group>
-        <form-group style="display: flex">
-            <label for="password">Name</label>
-            <input type="password" name="password">
-        </form-group>
-        <button type="submit">Login</button>
+        {{-- @if ($errors->any())
+            <div class="text-red-600">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif --}}
+        <label for="email" class=" text-slate-900 mb-1">Email</label> <br>
+        <input type="email" name="email" class="bg-slate-200 py-1 mb-2 px-2 w-full"> <br>
+        @if ($errors->any() && $errors->email)
+            <p class="text-red-600 mb-4">{{ $errors->first('email') }}</p>
+        @endif
+        <label for="password" class=" text-slate-900">Password</label> <br>
+        <input type="password" name="password" class="bg-slate-200 py-1 px-2 w-full">
+        @if ($errors->any() && $errors->password)
+            <p class="text-red-600">{{ $errors->first('password') }}</p>
+        @endif
+        <br>
+        <button type="submit" class=" text-pink-100 cursor-pointer bg-black my-6 py-2 px-4 w-full">Login</button>
+        <p class="text-center cursor-pointer">Register</p>
     </form>
 </body>
 
--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,7 @@ Route::middleware('auth')->group(function () {
     // logout
     Route::get('logout', [UserAuth::class, 'logout']);
 });
+
 // login
 Route::get('login', [UserAuth::class, 'login'])->name('login');
 Route::post('login', [UserAuth::class, 'proccess_login']);
@@ -43,3 +44,27 @@ Route::post('verif-email-changePassword', [UserAuth::class, 'code_changePassword
 
 Route::get('changePassword', [UserAuth::class, 'changePassword']);
 Route::post('changePassword', [UserAuth::class, 'proccess_changePassword']);
+
+// Pages
+//  Client
+// Home
+Route::get('/home', function () {
+    return view('Auth.login');
+})->middleware('auth');
+
+
+// Admin
+Route::middleware('auth', 'role:admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('Admin.Dashboard');
+    });
+    // Lowongan
+    Route::resource('lowongan', LowonganController::class);
+    // Apply
+    Route::get('apply', [ApplyJobController::class, 'index']);
+    Route::get('apply-create', [ApplyJobController::class, 'create']);
+    Route::post('apply', [ApplyJobController::class, 'store']);
+    Route::get('apply-show', [ApplyJobController::class, 'show']);
+    Route::post('apply-status-reject', [ApplyJobController::class, 'reject']);
+    Route::post('apply-status-konfirm', [ApplyJobController::class, 'konfirm']);
+});
