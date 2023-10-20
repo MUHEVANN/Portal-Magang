d8ea247a5a3cb47e53a638dd13214c3f9659baeb
DEVELOPER JETORBIT <jet-dev@MacBook-Pro-DEVELOPER.local>
2023-10-20 10:27:53 +0700

    for handling build assets

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -26,7 +26,12 @@
     <link rel="stylesheet" type="text/css" href="https://unpkg.com/trix@2.0.0/dist/trix.css">
     <script type="text/javascript" src="https://unpkg.com/trix@2.0.0/dist/trix.umd.min.js"></script>
 
-    @vite(['resources/js/app.js'])
+    
+    @if(env('APP_ENV') == 'local')
+        @vite(['resources/js/app.js'])
+    @else 
+        <script src="{{ asset('build/assets/app-e596d21f.js') }}"></script>
+    @endif
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
@@ -48,7 +53,12 @@
     <link rel="stylesheet" href="{{ asset('template/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}" />
 
     <link rel="stylesheet" href="{{ asset('template/assets/vendor/libs/apex-charts/apex-charts.css') }}" />
-    @vite(['resources/css/dashboard.css'])
+    @if(env('APP_ENV') == 'local')
+        @vite(['resources/css/dashboard.css'])
+    @else 
+        <link rel="stylesheet" href="{{ asset('build/assets/dashboard-b2782425.css') }}"
+        class="template-customizer-core-css" />
+    @endif
     <!-- Page CSS -->
 
     <!-- Helpers -->
